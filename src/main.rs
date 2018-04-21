7d8499dece0cbff52b1320903ce5eecdaa22f5b3
Jan Weidner <jw3126@gmail.com>
2018-04-21 17:42:01 +0200

    mvp

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate clap;
+use std::io::prelude::*;
+use clap::{Arg};
+use std::io::{BufReader};
+use std::fs::{File};
+use std::path::{Path};
+
+fn doit(pattern:&str, filepath:&Path) {
+    let file = File::open(filepath).expect("Cannot open filepath");
+    let buf = BufReader::new(file);
+    for (i,line) in buf.lines().enumerate() {
+        let s = line.unwrap();
+        if s.contains(pattern) {
+            println!("{} {}",i,s)
+        }
+    };
+}
+
+fn main() {
+    
+    let matches = clap::App::new("toygrep")
+                          .version("0.1")
+                          .author("Jan Weidner <jw3126@gmail.com>")
+                          .about("Simple grep clone.")
+                          .arg(Arg::with_name("PATTERN")
+                               .help("Pattern that should be searched for")
+                               .required(true)
+                               .index(1))
+                          .arg(Arg::with_name("FILE")
+                               .required(true)
+                               .index(2)
+                               .help("File that should be searched"))
+                          .get_matches();
+
+    let pattern = matches.value_of("PATTERN").unwrap();
+    let filename = matches.value_of("FILE").unwrap();
+    let filepath = Path::new(filename);
+    doit(pattern, filepath);
+}
